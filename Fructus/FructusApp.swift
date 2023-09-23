3d9dcf4080c544f34442f3157399cf6b7caf50e7
Arman Khan <work.armankhan@gmail.com>
2023-09-23 16:38:14 +0530

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Arman Khan on 23/09/23.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
